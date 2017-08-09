b49b08e8ef20b9ec715ae5d6bb6f035b080e8d36
requireonce <hspringsun@gmail.com>
2017-08-09 23:21:38 +0800

    8.9
    8.9

---
+++ src/main/java/com/javaabc/lombok/LombokDemo.java
@@ -0,0 +1,11 @@
+package com.javaabc.lombok;
+
+public class LombokDemo {
+    public static void main(String[] args) {
+        User user = new User();
+
+       // System.out.println(user.getId());
+
+    }
+}
+
